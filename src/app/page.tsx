76be96b1e11be8893e1173cd65243612717c427c
elijah629 <62805599+elijah629@users.noreply.github.com>
2025-11-22 14:07:06 -0800

    add: tech

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Footer } from "@/components/footer";
 import { Banner } from "@/components/hackclub-banner";
 import { Hero } from "@/components/hero";
+import { Technologies } from "@/components/technologies";
 import { Work } from "@/components/work";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Banner />
       <Hero />
       <Work />
+      <Technologies />
       <Footer />
     </>
   );
